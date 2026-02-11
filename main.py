5b1094f13403006a9858af2771f32fed7bc532b6
jardelgalvao10 <seu-email-do-github@exemplo.com>
2026-02-11 07:14:58 -0300

    fix: conexao openai

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from dotenv import load_dotenv
 
 # 1. CONFIGURAÇÕES INICIAIS
 load_dotenv()
-client_openai = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+chave_openai = st.secrets["OPENAI_API_KEY"]
+client_openai = OpenAI(api_key=chave_openai)
 
 st.set_page_config(page_title="Jardas - Seu Tutor de Inglês", page_icon="🎓")
 st.title("Olá sou o Jardas...")
